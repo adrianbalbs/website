ac72c75a3d9594f7a3750ee046a951588fe8a593
Adrian Balbalosa <adrianbalbs@pm.me>
2026-08-09 15:26:38 +1000

    yet another personal website attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [{
+    provider: fontProviders.google(),
+    name: "Instrument Serif",
+    cssVariable: "--font-instrument-serif",
+    weights: [400],
+    styles: ['normal', 'italic'],
+    subsets: ['latin'],
+  }]
+});
