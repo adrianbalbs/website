e5dc9511974bc01572dfa53c2994920a13da70fb
Adrian Balbalosa <adrianbalbs@pm.me>
2026-08-09 16:21:39 +1000

    add icons, also fix hover for header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   fonts: [{
@@ -10,5 +12,7 @@ export default defineConfig({
     weights: [400],
     styles: ['normal', 'italic'],
     subsets: ['latin'],
-  }]
+  }],
+
+  integrations: [icon()]
 });
